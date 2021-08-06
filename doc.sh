437cd34dd98d78a7187182b0ae159ca59797868c
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-08-06 18:03:50 -0300

    - Fixed doc.sh

--- doc.sh
+++ doc.sh
@@ -3,6 +3,6 @@
 #asciidoctor -D docs administration-guide.adoc 
 #asciidoctor -D docs connectors.adoc 
 #asciidoctor -D docs installation-guide.adoc
-asciidoctor-pdf --base-dir docs docs/administration-guide.adoc -a imagesdir=/ -o turing/turing-administration-guide.pdf
-asciidoctor-pdf --base-dir docs docs/connectors.adoc -a imagesdir=/ -o turing/turing-connectors.pdf
-asciidoctor-pdf --base-dir docs docs/installation-guide.adoc -a imagesdir=/ -o turing/turing-installation-guide.pdf
+asciidoctor-pdf --base-dir docs docs/turing-administration-guide.adoc -a imagesdir=/ -o turing/turing-administration-guide.pdf
+asciidoctor-pdf --base-dir docs docs/turing-connectors.adoc -a imagesdir=/ -o turing/turing-connectors.pdf
+asciidoctor-pdf --base-dir docs docs/turing-installation-guide.adoc -a imagesdir=/ -o turing/turing-installation-guide.pdf
