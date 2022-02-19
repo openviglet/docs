bef19b69cba06807328a1f1b19c23f7b99acbe40
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2022-02-19 13:00:57 -0300

    - Turing 0.3.6

--- doc.sh
+++ doc.sh
@@ -3,10 +3,15 @@
 asciidoctor-pdf --base-dir docs docs/shio-developer-guide.adoc -a imagesdir=/ -o shio/shio-developer-guide.pdf
 asciidoctor-pdf --base-dir docs docs/shio-installation-guide.adoc -a imagesdir=/ -o shio/shio-installation-guide.pdf
 
-asciidoctor-pdf --base-dir docs docs/turing/0.3.5/turing-administration-guide.adoc -a imagesdir=/ -o turing/0.3.5/turing-administration-guide-0.3.5.pdf
+asciidoctor-pdf --base-dir docs docs/turing/0.3.5/turing-administration-guide.adoc -a imagesdir=/ -o turing/turing-administration-guide-0.3.5.pdf
 asciidoctor-pdf --base-dir docs docs/turing/0.3.5/turing-connectors.adoc -a imagesdir=/ -o turing/turing-connectors-0.3.5.pdf
-asciidoctor-pdf --base-dir docs docs/turing/0.3.5/turing-installation-guide.adoc -a imagesdir=/ -o turing/0.3.5/turing-installation-guide-0.3.5.pdf
-asciidoctor-pdf --base-dir docs docs/turing/0.3.5/turing-developer-guide.adoc -a imagesdir=/ -o turing/0.3.5/turing-developer-guide-0.3.5.pdf
+asciidoctor-pdf --base-dir docs docs/turing/0.3.5/turing-installation-guide.adoc -a imagesdir=/ -o turing/turing-installation-guide-0.3.5.pdf
+asciidoctor-pdf --base-dir docs docs/turing/0.3.5/turing-developer-guide.adoc -a imagesdir=/ -o turing/turing-developer-guide-0.3.5.pdf
+
+asciidoctor-pdf --base-dir docs docs/turing/0.3.6/turing-administration-guide.adoc -a imagesdir=/ -o turing/turing-administration-guide-0.3.6.pdf
+asciidoctor-pdf --base-dir docs docs/turing/0.3.6/turing-connectors.adoc -a imagesdir=/ -o turing/turing-connectors-0.3.5.pdf
+asciidoctor-pdf --base-dir docs docs/turing/0.3.6/turing-installation-guide.adoc -a imagesdir=/ -o turing/turing-installation-guide-0.3.6.pdf
+asciidoctor-pdf --base-dir docs docs/turing/0.3.6/turing-developer-guide.adoc -a imagesdir=/ -o turing/turing-developer-guide-0.3.6.pdf
 
 asciidoctor-pdf --base-dir docs docs/vecchio-developer-guide.adoc -a imagesdir=/ -o vecchio/vecchio-getting-started.pdf
 asciidoctor-pdf --base-dir docs docs/vecchio-getting-started.adoc -a imagesdir=/ -o vecchio/vecchio-getting-started.pdf
--- docs/_documentation/turing_5_presentation.markdown
+++
@@ -1,6 +0,0 @@
----
-title: Presentation
-description: Turing AI Overview Presentation.
-docurl: /turing/0.3.5/presentation/presentation.pdf
-product: turing
----
